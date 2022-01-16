9ef258874649918634d123e2a9c65caf71bccb7d
rixcx <adts0415.ovo@gmail.com>
2022-01-16 23:00:44 +0900

    [ css ] fix support responsive

--- components/header.js
+++ components/header.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import styles from '../styles/Header.module.scss';
 
 export default function Header() {
   return (
   <header className={styles.header}>
-    <h1>r.blog</h1>
+<h1>
+  <Link href="/">
+    <a>
+      r.blog
+    </a>
+  </Link>
+</h1>
   </header>
   )
 }
