6431d1b3310455a55516cdc7ba049fc796b090ef
rixcx <adts0415.ovo@gmail.com>
2022-01-23 22:08:58 +0900

    [ css ] set SyntaxHighlight

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,23 +1,20 @@
 import { client } from "../../libs/client";
 import Date from "../../libs/date";
+import cheerio from 'cheerio';
+import hljs from 'highlight.js';
 
 import styles from '../../styles/Blog-detail.module.scss';
+import 'highlight.js/styles/hybrid.css';
 
-export default function blogId({ blog }) {
+export default function blogId({ blog, highlightedBody }) {
   return (
     <div className={styles.main}>
       <div className={styles.header}>
         <h1 className={styles.title}>{blog.title}</h1>
         <p className={styles.time}><Date dateString={blog.date} /></p>
       </div>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${blog.body}`,
-        }}
-        className={styles.body}
-      />
+      <div dangerouslySetInnerHTML={{ __html: highlightedBody }} className={styles.body}></div>
     </div>
-    
   );
 }
 
@@ -34,9 +31,17 @@ export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
 
+  const $ = cheerio.load(data.body);
+  $("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass("hljs");
+  });
+
   return {
     props: {
       blog: data,
+      highlightedBody: $.html(),
     },
   };
 };
