6a4f29cd813c0ccf99252448db788375e2b135b8
rixcx <adts0415.ovo@gmail.com>
2022-01-26 23:07:03 +0900

    [all] add gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,34 @@
 import '../styles/global/reset.css'
 import '../styles/global/global.scss'
 import Layout from '../components/layout'
+import { useRouter } from "next/router";
+import React, { useEffect } from 'react'
+import { existsGaId, pageview } from '../libs/gtag'
+import GoogleAnalytics from '../components/GoogleAnalytics'
 
 function MyApp({ Component, pageProps }) {
+ 
+  const router = useRouter()
+  useEffect(() => {
+    if (!existsGaId) {
+      return
+    }
+    const handleRouteChange = (path) => {
+      pageview(path)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+ 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GoogleAnalytics />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
