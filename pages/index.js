d635ae77f81427062a774a6402032cf31442ca9c
rixcx <adts0415.ovo@gmail.com>
2022-01-27 22:14:30 +0900

    [index] apiget only title

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Index({ blog }) {
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
+  const data = await client.get({ endpoint: "blog", queries: {fields: 'id,title,date'} });
 
   return {
     props: {
